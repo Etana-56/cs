171d178bb64fac38beb598d21a425b292764994b
Maryam Hadi <athyraytana@gmail.com>
2026-03-17 04:34:46 +0300

    Create Q2.py

---
+++ H.W/Q2.py
@@ -0,0 +1,11 @@
+# Q2
+num = int(input("Enter a positive integer: "))
+
+sum_digits = 0
+
+while num > 0:
+    digit = num % 10
+    sum_digits += digit
+    num = num // 10
+
+print("Sum of digits:", sum_digits)
